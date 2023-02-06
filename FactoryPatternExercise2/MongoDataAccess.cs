0c992cbcb7869d47117eaefdf844ee7f2a786f01
tyler ambrose <blitzinc98@yahoo.com>
2023-02-06 05:44:29 -0500

    done

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product() {Name = "Xbox", Price = 500 },
+            new Product() {Name = "PS5", Price = 400 },
+            new Product() {Name = "Switch", Price = 300 },
+            new Product() {Name = "PC", Price = 1000 },
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from Mongo Data Access.");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from Mongo Data Access.");
+        }
+    }
+}
